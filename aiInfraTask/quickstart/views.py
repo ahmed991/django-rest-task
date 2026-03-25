91b5471bc3c9098520a47588cebcb96c5cf0c9aa
AhmedMavisoft <ahmed@mavisoft.com>
2026-03-25 14:06:11 +0100

    Spatial query without geojson feature type

--- aiInfraTask/quickstart/views.py
+++ aiInfraTask/quickstart/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, generics
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from django.contrib.gis.geos import GEOSGeometry
+from aiInfraTask.quickstart.seriallizers import GroupSerializer, UserSerializer, MunicipalitiesSerializer
+from .models import Municipalities
+from django.shortcuts import render
 
-from aiInfraTask.quickstart.seriallizers import GroupSerializer, UserSerializer
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,3 +27,52 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by("name")
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+
+class MunicipalitiesList(generics.ListCreateAPIView):
+    serializer_class = MunicipalitiesSerializer
+
+
+
+    def get_queryset(self):
+        queryset = Municipalities.objects.all()
+
+        bbox = self.request.query_params.get('in_bbox', None)
+
+        if(bbox):
+            min_lng, min_lat, max_lng, max_lat = map(float, bbox.split(','))
+
+            bbox_geom = GEOSGeometry(
+                f'POLYGON(({min_lng} {min_lat}, {max_lng} {min_lat}, {max_lng} {max_lat},{min_lng} {max_lat}, {min_lng} {min_lat}))',
+                srid=4326
+            )
+            print(bbox_geom)
+
+            queryset = queryset.filter(location_within=bbox_geom)
+        
+        return queryset
+
+    
+    # queryset = Municipalities.objects.all()
+    # serializer_class = MunicipalitiesSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# class MunicipalitiesCreate(generics.CreateAPIView):
+#     queryset = Municipalities.objects.all()
+#     serializer_class = MunicipalitiesSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# class MunicipalitiesDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Municipalities.objects.all()
+#     serializer_class = MunicipalitiesSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+# class MunicipalitiesUpdate(generics.UpdateAPIView):
+#     queryset = Municipalities.objects.all()
+#     serializer_class = MunicipalitiesSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+# class 
