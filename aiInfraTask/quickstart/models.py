8a00898a4d057022e33c7685f4436fb4c7ed7276
ahmed <ahmed@scanplan.com>
2026-03-25 04:08:18 +0100

    Define municipality model table

---
+++ aiInfraTask/quickstart/models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+from django.contrib.gis.db import models
+
+# Create your models here.
+class Municipalities(models.Model):
+    id  = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    geom = models.GeometryField()
+
+    def __str__(self):
+        return self.name
