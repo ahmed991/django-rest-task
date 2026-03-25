ba24dc7f7daad4600c97d83542e0d94fa063f19f
AhmedMavisoft <ahmed@mavisoft.com>
2026-03-25 20:17:32 +0100

    Create and register router for municipalities view

---
+++ aiInfraTask/quickstart/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+
+
+
+from aiInfraTask.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r"municipalities", views.MunicipalityViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
