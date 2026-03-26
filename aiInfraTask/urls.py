ae87c2320aa3ae7c738b6d391898a08a6a815a2b
AhmedMavisoft <ahmed@mavisoft.com>
2026-03-26 10:43:22 +0100

    Add redirect for default  URL

--- aiInfraTask/urls.py
+++ aiInfraTask/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
@@ -21,5 +23,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("api/token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url = '/api'),name="Home page"),
+
 
 ]
